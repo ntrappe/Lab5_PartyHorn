fc82b11f5d0671416ef457945de558917801184d
ntrappe <ntrappe@ucsd.edu>
2021-02-04 18:00:11 -0800

    vol level set appropriately now

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,35 +4,31 @@
 //myHeading.textContent = 'Hello World!';
 // TODO
 
-/*
-var vol_input = document.querySelector('[id="volume-controls"]');
-console.log(vol_input.innerHTML);
-myHeading.textContent = vol_input.innerHTML;*/
-
-
+// audio object
+var audio_obj = document.getElementById('horn-sound');
 
 // Get value of input number object
- var vol_box_input = document.getElementById('volume-number').addEventListener('change', get_vol);
+var vol_box_input = document.getElementById('volume-number').addEventListener('change', get_vol);
 
  // Get value of input slider object
- var vol_slider_input = document.getElementById('volume-slider').addEventListener('change', get_vol);
+var vol_slider_input = document.getElementById('volume-slider').addEventListener('change', get_vol);
 
- function get_vol(e) {
+function get_vol(e) {
+    rel_vol = e.target.value / 100;
     var texts = document.querySelector('h2');
-    texts.textContent = e.target.value;
- }
+    texts.textContent = rel_vol;
+    // set audio volume level accordingly
+    audio_obj.volume = rel_vol;
+    alert(audio_obj.volume);
+}
 
- // audio object
- var audio_obj = document.getElementById('horn-sound');
- // set audio volume level accordingly
- audio_obj.volume = 0.1;
 
- // stop honk button from reloading the page and play the sound
- var sound_button = document.getElementById('honk-btn').addEventListener('click', function(event) {
+//alert(audio_obj.volume);
+// stop honk button from reloading the page and play the sound
+var sound_button = document.getElementById('honk-btn').addEventListener('click', function(event) {
     event.preventDefault();
-    
     audio_obj.play();
- });
+});
 
 
 
