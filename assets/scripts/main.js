4b8701ade2666e5e6edf29590284749124e2ea86
ntrappe <ntrappe@ucsd.edu>
2021-02-05 00:17:52 -0800

    two way bind working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,33 @@
 // main.js
 // replaces h1 text "110 Lab - Party Horn"
-//const myHeading = document.querySelector('h1');
+//const myHeading = document.querySelector('h2');
 //myHeading.textContent = 'Hello World!';
 // TODO
 
 // audio object
 var audio_obj = document.getElementById('horn-sound');
+// volume input box
+var vol_box_input = document.getElementById('volume-number');
+// volume input slider
+var vol_slider_input = document.getElementById('volume-slider');
 
-// Get value of input number object
-var vol_box_input = document.getElementById('volume-number').addEventListener('change', get_vol);
+vol_box_input.addEventListener('change', input_vol);
 
- // Get value of input slider object
-var vol_slider_input = document.getElementById('volume-slider').addEventListener('change', get_vol);
+// gets input volume and sets it for slider
+function input_vol(e) {
+    rel_vol = e.target.value / 100;     // change to [0,1] from [0.100]
+    audio_obj.volume = rel_vol;         // set volume level
+    vol_slider_input.value = 50;        // update slider to match
+}
 
-function get_vol(e) {
+
+vol_slider_input.addEventListener('change', slider_vol);
+
+// gets slider volume and sets it for input
+function slider_vol(e) {
     rel_vol = e.target.value / 100;
-    var texts = document.querySelector('h2');
-    texts.textContent = rel_vol;
-    // set audio volume level accordingly
     audio_obj.volume = rel_vol;
+    vol_box_input.value = e.target.value;
     alert(audio_obj.volume);
 }
 
