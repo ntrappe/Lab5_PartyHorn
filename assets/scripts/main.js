d549b71f2b5b43564205bac853921157615efff4
ntrappe <ntrappe@ucsd.edu>
2021-02-05 13:29:03 -0800

    Part 1 done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,53 +1,80 @@
 // main.js
-// replaces h1 text "110 Lab - Party Horn"
-//const myHeading = document.querySelector('h2');
-//myHeading.textContent = 'Hello World!';
-// TODO
 
+// Inputs/Audio Obj
 var audio_obj = document.getElementById('horn-sound');              // audio object
 var vol_box_input = document.getElementById('volume-number');       // volume input box
 var vol_slider_input = document.getElementById('volume-slider');    // volume input slider
 
+// Icons
 var vol_icon = document.getElementById('volume-image');             // volume image icon
 var horn_icon = document.getElementById('sound-image');             // horn icon
 var horn_sound = document.getElementById('horn-sound');             // horn sound
 
+// Buttons
+var sound_button = document.getElementById('honk-btn');             // 'Honk' button
 var opt_air = document.getElementById('radio-air-horn');            // air horn button
 var opt_car = document.getElementById('radio-car-horn');            // car horn button
 var opt_party = document.getElementById('radio-party-horn');        // party horn button
 
-// user inputs
+/* 
+ * Gets user input from input box. Checks if input is [0,100] otherwise defaults to 100.
+ * Divides input by 100 to set audio object's volume [0,1] and updates slider
+ * volume icon, and sound button
+ */
 vol_box_input.addEventListener('change', function(e) {
-    rel_vol = e.target.value / 100;             // change to [0,1] from [0.100]
+    // invalid input checking
+    if (e.target.value < 0 || e.target.value > 100) {
+        alert('Please enter a volume level between 0 and 100. Using default instead.');
+        vol_box_input.value = 100;
+        rel_vol = 1;
+    } else {
+        rel_vol = e.target.value / 100;         // change to [0,1] from [0.100]
+    }
     audio_obj.volume = rel_vol;                 // set volume level
     vol_slider_input.value = e.target.value;    // update slider to match
     set_vol_img(e.target.value);
+    set_sound_btn(e.target.value);
 });
 
+/* 
+ * Gets user input from slider. Divides input by 100 to set audio object's volume [0,1]
+ * and updates input box, volume icon, and sound button
+ */
 vol_slider_input.addEventListener('change', function(e) {
-    rel_vol = e.target.value / 100;
-    audio_obj.volume = rel_vol;
-    vol_box_input.value = e.target.value;  
+    rel_vol = e.target.value / 100;             // changes to [0.1] from [0,100]
+    audio_obj.volume = rel_vol;                 // set volume level
+    vol_box_input.value = e.target.value;       // update text entry to match
     set_vol_img(e.target.value);
+    set_sound_btn(e.target.value);
 });
 
+/*
+ * Updates horn icon and sound to match selection of Air Horn
+ */
 opt_air.addEventListener('click', function(e) {
     horn_icon.src = "./assets/media/images/air-horn.svg";
     horn_sound.src = "./assets/media/audio/air-horn.mp3";
 });
 
+/*
+ * Updates horn icon and sound to match selection of Car Horn
+ */
 opt_car.addEventListener('click', function(e) {
     horn_icon.src = "./assets/media/images/car.svg";
     horn_sound.src = "./assets/media/audio/car-horn.mp3";
 });
 
+/*
+ * Updates horn icon and sound to match selection of Party Horn
+ */
 opt_party.addEventListener('click', function(e) {
     horn_icon.src = "./assets/media/images/party-horn.svg";
     horn_sound.src = "./assets/media/audio/party-horn.mp3";
 });
 
-
-// volume icon matches the volume number
+/*
+ * Set volume icon to match the volume number
+ */
 function set_vol_img(vol) {
     if (vol == 0) {
         vol_icon.src = "./assets/media/icons/volume-level-0.svg";
@@ -60,10 +87,22 @@ function set_vol_img(vol) {
     }
 }
 
+/*
+ * Disable sound button is volume is muted
+ */
+function set_sound_btn(vol) {
+    // if volume is 0, disable sound button
+    if (vol == 0) {
+        sound_button.disabled = true;
+    } else {
+        sound_button.disabled = false;
+    }
+}
 
-//alert(audio_obj.volume);
-// stop honk button from reloading the page and play the sound
-var sound_button = document.getElementById('honk-btn').addEventListener('click', function(event) {
+/* 
+ * Stop honk button from reloading the page and play the sound
+ */
+sound_button.addEventListener('click', function(event) {
     event.preventDefault();
     audio_obj.play();
 });
